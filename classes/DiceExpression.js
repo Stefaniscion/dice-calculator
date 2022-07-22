337ba2cdf39c486da42cc4e29e4254faa96643a4
Stefano Di Pasquale <stefaniscion@gmail.com>
2022-07-22 12:45:13 +0200

    prettied DiceExpression.js

--- classes/DiceExpression.js
+++ classes/DiceExpression.js
@@ -1,14 +1,14 @@
 /**
  * Accept D&D style dice expressions and calculates them.
  */
-class DiceExpression{
+class DiceExpression {
 
     /**
      * Creates a DiceExpression object.
      * @constructor 
      * @param {string} expression A D&D style dice expression.
      */
-    constructor(expression){
+    constructor(expression) {
         expression = this.#trimExpression(expression);
         this.expression = expression;
     }
@@ -18,9 +18,9 @@ class DiceExpression{
      * @method #trimExpression 
      * @param {String} expression A D&D style dice expression.
      */
-    #trimExpression(expression){
+    #trimExpression(expression) {
         expression = expression.toLowerCase();
-        expression = expression.replaceAll(' ','');
+        expression = expression.replaceAll(' ', '');
         return expression;
     }
 
@@ -28,13 +28,13 @@ class DiceExpression{
      * Returns the result of the Dice Expression by rolling the dice.
      * @method getRoll
      */
-    getRoll(){
+    getRoll() {
         let dicePattern = /[0-9]*d[0-9]+/g;
         let matchedDice = this.expression.match(dicePattern);
-        let rolledExpression =  this.expression
-        for(let i in matchedDice){
+        let rolledExpression = this.expression
+        for (let i in matchedDice) {
             let rolledDice = this.#roll(matchedDice[i]);
-            rolledExpression = rolledExpression.replaceAll(matchedDice[i],rolledDice);
+            rolledExpression = rolledExpression.replaceAll(matchedDice[i], rolledDice);
         }
         let result = this.#calculate(rolledExpression);
         return result;
@@ -44,13 +44,13 @@ class DiceExpression{
      * Returns the result of the expression by getting the mean of the dice.
      * @method getMean
      */
-    getMean(){
+    getMean() {
         let dicePattern = /[0-9]*d[0-9]+/g;
         let matchedDice = this.expression.match(dicePattern);
-        let rolledExpression =  this.expression
-        for(let i in matchedDice){
+        let rolledExpression = this.expression
+        for (let i in matchedDice) {
             let rolledDice = this.#mean(matchedDice[i]);
-            rolledExpression = rolledExpression.replaceAll(matchedDice[i],rolledDice);
+            rolledExpression = rolledExpression.replaceAll(matchedDice[i], rolledDice);
         }
         let result = this.#calculate(rolledExpression);
         return result;
@@ -62,14 +62,14 @@ class DiceExpression{
      * @param {string} diceString a single-dice expression.
      * @return {number} Rolled result of single-dice expression.
      */
-    #roll(diceString){
-        let dice = diceString.split("d"); 
-        if(dice[0] == ""){dice[0] = "1"}
+    #roll(diceString) {
+        let dice = diceString.split("d");
+        if (dice[0] == "") { dice[0] = "1" }
         dice[0] = parseInt(dice[0]);
         dice[1] = parseInt(dice[1]);
         let diceSum = 0;
-        for(let j=0;j<dice[0];j++){
-            let rolledDice = Math.floor(Math.random() * dice[1])+1;
+        for (let j = 0; j < dice[0]; j++) {
+            let rolledDice = Math.floor(Math.random() * dice[1]) + 1;
             diceSum = diceSum + rolledDice;
         }
         return diceSum;
@@ -81,23 +81,23 @@ class DiceExpression{
      * @param {string} diceString a single-dice expression.
      * @return {number} Mean of single-dice expression.
      */
-    #mean(diceString){
+    #mean(diceString) {
         let dice = diceString.split("d");
-        if(dice[0] == ""){dice[0] = "1"}
+        if (dice[0] == "") { dice[0] = "1" }
         dice[0] = parseInt(dice[0]);
         dice[1] = parseInt(dice[1]);
-        let diceSum = (Math.floor(dice[1]/2) + 1) * dice[0];
+        let diceSum = (Math.floor(dice[1] / 2) + 1) * dice[0];
         return diceSum;
     }
-    
+
     /**
      * Returns the sum of the expression.
      * @method #calculate
      * @param {string} expression a math expression.
      * @return {number} Result of the math expression.
      */
-    #calculate(expression){
-        let result =  eval(expression);
+    #calculate(expression) {
+        let result = eval(expression);
         return result;
     }
 
